ef33272d6bce8f6e678d8d45f7bf09dd283aeac0
Tulio Gabriel <tulliocba@gmail.com>
2021-09-17 09:56:50 -0300

    Added skeleton

--- src/app/features/UserTopTags.tsx
+++ src/app/features/UserTopTags.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 import styled from "styled-components";
 import { withBoundaryError } from "../../core/hoc/withBondaryError";
 import { Metric } from "../../sdk/@types";
@@ -11,17 +12,25 @@ export const UserTopTags = withBoundaryError(() => {
     useEffect(() => {
         MetricService.getTopThreeTags()
             .then(setTopTags)
-    }, [])
+    }, []);
+
+    if (!topTags.length) {
+        return <Wrapper style={{ height: 123 }}>
+            <Skeleton height={88} width={88} circle />
+            <Skeleton height={88} width={88} circle />
+            <Skeleton height={88} width={88} circle />
+        </Wrapper>
+    }
 
     return (
         <Wrapper>
             {
                 topTags.map((tag, index) => (
                     <CircleChart
-                        key={tag.tagName} 
-                        progress={Math.ceil(tag.percentage)} 
-                        size={88} 
-                        caption={tag.tagName} 
+                        key={tag.tagName}
+                        progress={Math.ceil(tag.percentage)}
+                        size={88}
+                        caption={tag.tagName}
                         theme={index === 0 ? 'primary' : 'default'} />
                 ))
             }
